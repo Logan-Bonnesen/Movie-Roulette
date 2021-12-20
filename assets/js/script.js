9f9cec8290463175ce6c76952f654acd8fec813c
Emily Burger <eburger939@gmail.com>
2021-12-20 15:14:39 -0600

    trying to pull data from API

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var imdbApiKey = 'k_flc35q5h'
+
+
+
+
+var queryUrl = 'https://imdb-api.com/en/API/SearchTitle/' + imdbApiKey +'/friday night lights/';
+fetch(queryUrl)
+.then (function (response) {
+    return response.json();
+})
+.then (function (data) {
+    console.log(data);
+
+    
+ for (var i=0; i < data.length; i++) {
+     var movieID = data.results[i]
+     console.log(movieID)
+ }
+});
+
+
