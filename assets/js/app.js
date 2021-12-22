9660b9c6032a7ba9223020be8a9ce0c510585c2a
Tom M <T0930@protonmail.com>
2021-12-22 14:12:56 -0700

    updating html, css, app.js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
-var imdbApiKey = 'k_flc35q5h'
-var watchModeApiKey = 'Ec21JVame9BKXdK9NqLodse4afGCe0nl4nOejc7w'
+// var imdbApiKey = 'k_flc35q5h'
 
-
+var useAll = '';
 
 var sButton = document.getElementById('sButton');
 sButton.addEventListener('click', function (event) {
@@ -9,50 +8,54 @@ sButton.addEventListener('click', function (event) {
     var movieTitle = document.getElementById('inlineFormInputName').value;
     console.log(movieTitle);
     // getTitle(movieTitle);
-    getTitle();
+    getTitle(); ///////////////// put back in
 })
 
 
+function getTitle() {
 
-// function getTitle() {
-// fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=27&page=1&output_language=en&language=en", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-// 		"x-rapidapi-key": "5905109ec5msh57a2f97f237f422p10f34bjsn176a53ffa732"
-// 	}
-// })
-// .then(function(response) {
-//         return response.json();
-// })
-// .then(function(data){
-// 	console.log(data);
-//     var boxes1 = ''
-//     var boxes2 = ''
-//     var boxes3 = ''
-//     var boxes4 = ''
-//     var boxes5 = ''
-//     var boxes6 = ''
-                 
-//                 for (let i = 0; i < 6; i++) {
-//                     var div = document.createElement('div')
-//                     div.classList = "column notification is-info"
-//                     boxes.appendChild(div)
+    var randPage = Math.floor(Math.random() *14)
+    var usePage = (randPage + 1);
+    console.log(randPage);
 
-//                     var title = data.results[i].title
-//                     var titleEl = document.createElement('p')
-//                     titleEl.textContent = title
-//                     div.appendChild(titleEl)
+fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=27&page=" + usePage + "&output_language=en&language=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+		"x-rapidapi-key": "5905109ec5msh57a2f97f237f422p10f34bjsn176a53ffa732"
+	}
+})
+.then(function(response) {
+        return response.json();
+})
+.then(function(data){
+	console.log(data);
 
+for (let i = 0; i < 6; i++) {
+     useAll += `<button class="launchModal">
+    <div class="column notification is-info">
+    boxes3 = <div class="tile is-child box">
+     <div class="title">${data.results[i].title}</div>
+     <div><img src=${data.results[i].posterURLs[185]}></div>
+    </div> 
+    </button>`
+}
+    document.getElementById('boxes').innerHTML = useAll;
 
-//                     var img = data.results[i].posterURLs[185]
-//                     console.log(img)
-//                     var imgEl = document.createElement('img')
-//                     imgEl.setAttribute('src', img)
-//                     div.appendChild(imgEl)
+    $(".launchModal").click(function() {
+        $(".modal").addClass("is-active");
+    });
+     $(".modal-close").click(function() {
+       $(".modal").removeClass("is-active");
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// var boxes1 = ''
+    // var boxes2 = ''
+    // var boxes3 = ''
+    // var boxes4 = ''
+    // var boxes5 = ''
+    // var boxes6 = ''  
 
 
                     // boxes1 = `<div class="tile is-vertical is-parent has-text-centered is-3">
@@ -81,10 +84,12 @@ sButton.addEventListener('click', function (event) {
                 // document.getElementById('smallBoxes4').innerHTML = boxes4;
                 // document.getElementById('smallBoxes5').innerHTML = boxes5;
                 // document.getElementById('smallBoxes6').innerHTML = boxes6;
-//                 }
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                })
                 
-// });
-// }
+});
+}
 
 
 // function getTitle(movieTitle) { ///// IMDB API CALL //////
