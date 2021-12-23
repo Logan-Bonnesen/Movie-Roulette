963e8b617393b81031d8d29fc2c4c3425c664556
Emily Burger <eburger939@gmail.com>
2021-12-23 11:21:24 -0600

    merging service and genre drop downs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var imdbApiKey = 'k_flc35q5h'
 var watchModeApiKey = 'Ec21JVame9BKXdK9NqLodse4afGCe0nl4nOejc7w'
+var launchButton = document.querySelector('.launchModal')
 
 
 
@@ -13,92 +14,117 @@ var watchModeApiKey = 'Ec21JVame9BKXdK9NqLodse4afGCe0nl4nOejc7w'
 // modal.removeClass = 'is-active'
 // });
 
-$(".launchModal").click(function() {
-     $(".modal").addClass("is-active");
-});  
-  $(".modal-close").click(function() {
-    $(".modal").removeClass("is-active");
+
+
+
+var sButton = document.getElementById('sButton');
+sButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('hello')
+    userInput();
+})
+
+
+function userInput() {
+
+    var service = $('#service').val();
+    console.log(service)
+    var genre;
+    var genreChoice = $('#genre').val();
+
+    if (genreChoice === 'Adventure'){
+        genre = 12
+    } else if(genreChoice === 'Biography') {
+        genre = 1
+    } else if(genreChoice === 'Drama'){
+        genre = 18
+    } else if(genreChoice === 'Horror'){
+        genre = 27
+    } else if(genreChoice === 'Action'){
+        genre = 28
+    } else if(genreChoice === 'Comedy'){
+        genre = 35
+    } else if(genreChoice === 'Thriller'){
+        genre = 53
+    }  else if(genreChoice === 'Crime'){
+        genre = 80
+    } else if(genreChoice === 'Mystery'){
+        genre = 9648
+    } else {
+        genre = 10749
+    }
+ console.log(genre)
+
+    getTitle(service, genre)
+
+}
+
+
+
+function getTitle(service, genre) {
+fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${service}&type=movie&genre=${genre}7&page=1&output_language=en&language=en`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+		"x-rapidapi-key": "5905109ec5msh57a2f97f237f422p10f34bjsn176a53ffa732"
+	}
+})
+.then(function(response) {
+        return response.json();
+})
+.then(function(data){
+	console.log(data);
+    
+                for (let i = 0; i < 6; i++) {
+                    var modalButton = document.createElement('button')
+                    modalButton.addClass = 'launchModal'
+                    boxes.appendChild(modalButton)
+
+
+                    var div = document.createElement('div')
+                    div.classList = "column notification is-info"
+                    modalButton.appendChild(div)
+
+                    var title = data.results[i].title
+                    var titleEl = document.createElement('p')
+                    titleEl.textContent = title
+                    div.appendChild(titleEl)
+
+
+                    var img = data.results[i].posterURLs[185]
+                    console.log(img)
+                    var imgEl = document.createElement('img')
+                    imgEl.setAttribute('src', img)
+                    div.appendChild(imgEl)
+
+                }
+launchModal(modalButton)
+                
 });
 
+}
+
+function launchModal(modalButton) {
+// var launch = document.querySelector(".launchModal")
+modalButton.addEventListener("click", function(){
+    var modal = document.querySelector(".modal")
+    modal.addClass = 'is-active'
+});
+var closeModal = document.querySelector('.modal-close')
+closeModal.addEventListener('click', function(){
+modal.removeClass = 'is-active'
+});
+}
 
 
-// var sButton = document.getElementById('sButton');
-// sButton.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     var movieTitle = document.getElementById('inlineFormInputName').value;
-//     console.log(movieTitle);
-//     // getTitle(movieTitle);
-//     getTitle();
-// })
-
-
-
-
-
-// function getTitle() {
-// fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=27&page=1&output_language=en&language=en", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-// 		"x-rapidapi-key": "5905109ec5msh57a2f97f237f422p10f34bjsn176a53ffa732"
-// 	}
-// })
-// .then(function(response) {
-//         return response.json();
-// })
-// .then(function(data){
-// 	console.log(data);
-//                 for (let i = 0; i < 6; i++) {
-//                     var div = document.createElement('div')
-//                     div.classList = "column notification is-info"
-//                     boxes.appendChild(div)
-
-//                     var title = data.results[i].title
-//                     var titleEl = document.createElement('p')
-//                     titleEl.textContent = title
-//                     div.appendChild(titleEl)
-
-
-//                     var img = data.results[i].posterURLs[185]
-//                     console.log(img)
-//                     var imgEl = document.createElement('img')
-//                     imgEl.setAttribute('src', img)
-//                     div.appendChild(imgEl)
-
-
-
-
-
-                    // boxes1 = `<div class="tile is-vertical is-parent has-text-centered is-3">
-                    // <div class="tile is-child box"></div>
-                    // <div class="title">${data.results[i].title}</div>
-                    // <div><img src=${data.results[i].posterURLs[185]}></div>`
-                    // boxes2 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[2].title}</div>
-                    // <div><img src=${data.results[2].posterURLs[185]}></div>`
-                    // boxes3 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[3].title}</div>
-                    // <div><img src=${data.results[3].posterURLs[185]}></div>`
-                    // boxes4 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[4].title}</div>
-                    // <div><img src=${data.results[4].posterURLs[185]}></div>`
-                    // boxes5 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[5].title}</div>
-                    // <div><img src=${data.results[5].posterURLs[185]}></div>`
-                    // boxes6 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[6].title}</div>
-                    // <div><img src=${data.results[6].posterURLs[185]}></div>`
-
-                // document.getElementById('boxes1').innerHTML = boxes1;
-                // document.getElementById('smallBoxes2').innerHTML = boxes2;
-                // document.getElementById('smallBoxes3').innerHTML = boxes3;
-                // document.getElementById('smallBoxes4').innerHTML = boxes4;
-                // document.getElementById('smallBoxes5').innerHTML = boxes5;
-                // document.getElementById('smallBoxes6').innerHTML = boxes6;
-//                 }
-                
+// $(".launchModal").click(function() {
+//     $(".modal").addClass("is-active");
+// });  
+//  $(".modal-close").click(function() {
+//    $(".modal").removeClass("is-active");
 // });
-// }
+
+
 
 
 // function getTitle(movieTitle) { ///// IMDB API CALL //////
