f3372db3e76a15707fd70b2a35981cd6d7b2f477
Tom M <T0930@protonmail.com>
2021-12-23 12:25:52 -0700

    updated JS and HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,59 @@
-// var imdbApiKey = 'k_flc35q5h'
-
+var imdbApiKey = 'k_flc35q5h'
+var imdbApiKey2 = 'k_5yosmfb0'
 var useAll = '';
 
 var sButton = document.getElementById('sButton');
 sButton.addEventListener('click', function (event) {
     event.preventDefault();
-    var movieTitle = document.getElementById('inlineFormInputName').value;
-    console.log(movieTitle);
-    // getTitle(movieTitle);
-    getTitle(); ///////////////// put back in
+
+    console.log('hello')
+    userInput();
 })
 
 
-function getTitle() {
+function userInput() {
+
+    var serv = $('#service').val();
+    var service =serv.toLowerCase()
+    console.log(service)
+    
+    var genre;
+    var genreChoice = $('#genre').val();
+
+    if (genreChoice === 'Adventure'){
+        genre = 12
+    } else if(genreChoice === 'Biography') {
+        genre = 1
+    } else if(genreChoice === 'Drama'){
+        genre = 18
+    } else if(genreChoice === 'Horror'){
+        genre = 27
+    } else if(genreChoice === 'Action'){
+        genre = 28
+    } else if(genreChoice === 'Comedy'){
+        genre = 35
+    } else if(genreChoice === 'Thriller'){
+        genre = 53
+    }  else if(genreChoice === 'Crime'){
+        genre = 80
+    } else if(genreChoice === 'Mystery'){
+        genre = 9648
+    } else {
+        genre = 10749
+    }
+ console.log(genre)
+
+    getTitle(service, genre)
+
+}
+
+function getTitle(service, genre) {
 
     var randPage = Math.floor(Math.random() *14)
     var usePage = (randPage + 1);
     console.log(randPage);
 
-fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=27&page=" + usePage + "&output_language=en&language=en", {
+        fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${service}&type=movie&genre=${genre}&page=${usePage}&output_language=en&language=en`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
@@ -32,64 +67,50 @@ fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&ser
 	console.log(data);
 
 for (let i = 0; i < 6; i++) {
-     useAll += `<button class="launchModal">
-    <div class="column notification is-info">
-    boxes3 = <div class="tile is-child box">
-     <div class="title">${data.results[i].title}</div>
-     <div><img src=${data.results[i].posterURLs[185]}></div>
-    </div> 
-    </button>`
+     useAll += `<button class=“launchModal”>
+     <div class=“column notification is-info is-one-third”>
+       <p class="title">${data.results[i].title}</p>
+       <figure class=“image is-4by3”>
+       <img src=${data.results[i].posterURLs[185]}>
+     </div>
+     </figure>
+     </button>`
 }
     document.getElementById('boxes').innerHTML = useAll;
 
     $(".launchModal").click(function() {
+        console.log('model working?')
         $(".modal").addClass("is-active");
     });
      $(".modal-close").click(function() {
        $(".modal").removeClass("is-active");
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// var boxes1 = ''
-    // var boxes2 = ''
-    // var boxes3 = ''
-    // var boxes4 = ''
-    // var boxes5 = ''
-    // var boxes6 = ''  
-
-
-                    // boxes1 = `<div class="tile is-vertical is-parent has-text-centered is-3">
-                    // <div class="tile is-child box"></div>
-                    // <div class="title">${data.results[i].title}</div>
-                    // <div><img src=${data.results[i].posterURLs[185]}></div>`
-                    // boxes2 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[2].title}</div>
-                    // <div><img src=${data.results[2].posterURLs[185]}></div>`
-                    // boxes3 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[3].title}</div>
-                    // <div><img src=${data.results[3].posterURLs[185]}></div>`
-                    // boxes4 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[4].title}</div>
-                    // <div><img src=${data.results[4].posterURLs[185]}></div>`
-                    // boxes5 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[5].title}</div>
-                    // <div><img src=${data.results[5].posterURLs[185]}></div>`
-                    // boxes6 = `<div class="tile is-child box">
-                    // <div class="title">${data.results[6].title}</div>
-                    // <div><img src=${data.results[6].posterURLs[185]}></div>`
-
-                // document.getElementById('boxes1').innerHTML = boxes1;
-                // document.getElementById('smallBoxes2').innerHTML = boxes2;
-                // document.getElementById('smallBoxes3').innerHTML = boxes3;
-                // document.getElementById('smallBoxes4').innerHTML = boxes4;
-                // document.getElementById('smallBoxes5').innerHTML = boxes5;
-                // document.getElementById('smallBoxes6').innerHTML = boxes6;
-
-                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                })
+    })
                 
 });
 }
+
+
+// function launchModal(modalButton) {
+// // var launch = document.querySelector(".launchModal")
+// modalButton.addEventListener("click", function(){
+//     var modal = document.querySelector(".modal")
+//     modal.addClass = 'is-active'
+// });
+// var closeModal = document.querySelector('.modal-close')
+// closeModal.addEventListener('click', function(){
+// modal.removeClass = 'is-active'
+// });
+// }
+
+
+// $(".launchModal").click(function() {
+//     $(".modal").addClass("is-active");
+// });  
+//  $(".modal-close").click(function() {
+//    $(".modal").removeClass("is-active");
+// });
+
+
 
 
 // function getTitle(movieTitle) { ///// IMDB API CALL //////
@@ -104,16 +125,53 @@ for (let i = 0; i < 6; i++) {
 //             var mTitle = data.results[0].title;
 //             console.log(mTitle);
 //             console.log(mID);
-//             var movieInfo = `
-//             <div class="movieTitle">Title ${data.results[0].title}</div>
-//             <div><img src=${data.results[0].image}></div>`
-//             document.getElementById('movieInfo').innerHTML = movieInfo;
-//             useTitle(mID);
+//             getInfo(mID);
+
 //         })
-//             for (var i = 0; i < data.results.length; i++) {
-//                 var movieSearch = data.results[i]
-//                 console.log(movieSearch);
-//                 //  console.log(data.results)
-//             }
-//         })
-// };
+            
+//         }
+
+
+
+
+
+
+
+
+// inlineFormInputName.addEventListener("keyup", function(event) {
+//     if (event.keyCode === 13) {
+//         event.preventDefault();
+//         document.getElementById("sButton").click();
+//     }})
+
+
+
+
+
+    // function getInfo(ttID) { ///// 2nd IMDB API CALL //////
+    //     // var ttID = "tt1375666"
+    //     // var mID = data.results[0].id;
+    //     // console.log(mID)
+    //     var queryUrl = 'https://imdb-api.com/en/API/Title/' + imdbApiKey2 + '/' + ttID;
+    //     fetch(queryUrl)
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (data) {
+    //             console.log(data);
+    //           var releaseDate = $("<p>").text(data.releaseDate);
+    //            $(".movie-info").append(releaseDate);
+    //           var runTime = $("<p>").text(data.runtimeStr);
+    //           $(".movie-info").append(runTime);
+    //           var imdbRating = $("<p>").text(data.imDbRating)
+    //           $(".movie-info").append(imdbRating);
+    //           var tagline = $("<p>").text(data.tagline);
+    //           $(".movie-info").append(tagline);
+
+    //           console.log(data.runtimeStr);
+    //           console.log(data.releaseDate);
+    //           console.log(data.imDbRating);
+    //           console.log(data.tagline);
+
+    //         })
+    //     }  
